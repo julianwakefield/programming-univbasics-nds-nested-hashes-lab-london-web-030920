3e4bb13c20af6f04d1f9e3e45b34f8af3edd96dc
Julian Wakefield <julianwakefield@outlook.com>
2020-03-03 00:22:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -39,7 +39,8 @@ def aoh_update(aoh, row, key, new_value)
 
 aoh[row][key] = new_value
 
-aoh_update
+return aoh_update
+
 end
 
   # Update the AoH data at row and key to have the value of new_value
